2dced325e2d4d6257b2732224d5bd5ca1bda7f80
Vijaypratapsing <122394731+Vijaypratapsing@users.noreply.github.com>
2026-01-02 23:55:36 +0530

    reverse array list

---
+++ Reverce.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Reverce {
+    static void reverse(ArrayList<Integer>arr){
+        int i=0;
+        int j=arr.size()-1;
+        while (i<j){
+            Integer temp=Integer.valueOf(i);
+
+        }
+    }
+}
